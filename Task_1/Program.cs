05a7d7b34e1d0026f79fa8fd032cb704dd86fc95
SergeyShamin <shamins61@yandex.ru>
2022-05-31 21:37:58 +0300

    finish task 1,2

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Пользователь с клавиатуры вводит M чисел, посчитайте сколько чисел больше 0 ввел пользователь. 
+*/ 
+
+int GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int number = int.Parse(Console.ReadLine());
+return number;
+}
+
+int m = 10;
+int count = 0;
+for (int i = 0; i < m ; i++)
+{
+    int number = GetNumber("Введите число: ");
+
+    if (number > 0)
+        {
+            count++;
+        }   
+}
+Console.WriteLine($"Пользователь ввел {count} положительных чисел");
---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая найдет точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, x = k2 * x + b2; значения b1, k1, b2, k2 задаются пользователем. 
+*/ // x = (b2 - b1) / (k1 - k2) - формула пересечения двух прямых
+
+double GetNumber(string msg)
+{
+    Console.WriteLine(msg);
+    double number = double.Parse(Console.ReadLine());
+return number;
+}
+
+double k1 = GetNumber("Введите число для точки k1: ");
+double b1 = GetNumber("Введите число для точки b1: ");
+double k2 = GetNumber("Введите число для точки k2: ");
+double b2 = GetNumber("Введите число для точки b2: ");
+
+if(k1 == k2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+return;        
+}
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения ({x}, {y})");
